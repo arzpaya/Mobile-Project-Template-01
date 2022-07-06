be82d239ca3d49e8c4c60d79a130de4097eaab60
Majid-Fathi <Majid@Mac>
2022-07-06 15:55:50 +0430

    first commit

---
+++ lib/mobile_project_template_01.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mobile_project_template_01;
+
+export 'src/mobile_project_template_01_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
